86d62cf94a5a4301b19a66095ff199dde0889f51
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:17:15 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_refinance_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Refinance Calculator by Calculator.iO";
 
 function display_ci_refinance_calculator(){
